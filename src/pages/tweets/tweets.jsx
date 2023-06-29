278eb9a198344c9417957b17affa5e9dc91fcec5
Dezertirr <125862316+Dezertirr@users.noreply.github.com>
2023-06-29 14:03:08 +0300

    upd

--- src/pages/tweets/tweets.jsx
+++ src/pages/tweets/tweets.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { fetchUsers } from '../../Services/tweetsAPI';
+import {
+  TweetsList,
+  TweetsItem,
+  TweetsPhoto,
+  TweetsLogo,
+  TweetsTopPhoto,
+  TweetsText,
+  TweetsBtn,
+} from './tweets.styled'
+import LogoGoIT from '../../images/Logo-GoIT.png'
+import TopPhoto from '../../images/TopPhoto.png'
 
 export function Tweets() {
   const [users, setUsers] = useState([]);
@@ -43,20 +54,24 @@ export function Tweets() {
   return (
     <div>
       <h1>Tweets</h1>
-      {users.map(user => (
-        <div key={user.id}>
-          <p>Name: {user.user}</p>
-          <p>Tweets: {user.tweets}</p>
-          <img src={user.avatar} alt={user.user} />
-          <p>Followers: {user.followers}</p>
-          <button
-            type="submit"
-            onClick={() => handleFollow(user.id)}
-          >
-            {user.isFollowing ? 'Following' : 'Follow'}
-          </button>
-        </div>
-      ))}
+      <TweetsList>
+        {users.map(user => (
+          <TweetsItem key={user.id}>
+            <TweetsLogo src={LogoGoIT} alt='GoITLogo'></TweetsLogo>
+            <TweetsTopPhoto src={TopPhoto} alt="Questions" />
+            <TweetsPhoto src={user.avatar} alt={user.user} />
+            <TweetsText>{user.tweets} Tweets </TweetsText>
+            <TweetsText>{user.followers} Followers</TweetsText>
+            <TweetsBtn
+              type="submit"
+              onClick={() => handleFollow(user.id)}
+              style={{ backgroundColor: user.isFollowing ? '#5CD3A8' : '' }}
+            >
+              {user.isFollowing ? 'Following' : 'Follow'}
+            </TweetsBtn>
+          </TweetsItem>
+        ))}
+      </TweetsList>
     </div>
   );
 }
--- src/pages/tweets/tweets.styled.js
+++ src/pages/tweets/tweets.styled.js
@@ -1,1 +1,58 @@
-import styled from 'styled-component'
+import styled from 'styled-components';
+
+export const TweetsList = styled.ul `
+display: flex;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-around;
+align-items: flex-start;
+list-style:none;
+gap: 16px
+`
+
+export const TweetsItem = styled.li `
+width:380px;
+height:460px;
+background: linear-gradient(114.99deg, #471CA9 -0.99%, #5736A3 54.28%, #4B2A99 78.99%);
+`
+
+export const TweetsPhoto = styled.img `
+width:80px;
+height:80px;
+display: flex;
+margin: 0 auto;
+border-radius:50%;
+object-fit: cover;
+`
+
+export const TweetsLogo = styled.img `
+padding: 20px 0 0 20px`
+
+export const TweetsTopPhoto = styled.img `
+padding: 0px 36px 0 36px;
+margin: -14px 0 0 0;`
+
+export const TweetsText = styled.p `
+color: #EBD8FF;
+font-size: 20px;
+font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+text-transform: uppercase;
+display: flex;
+justify-content: center;
+`
+
+export const TweetsBtn = styled.button `
+display: flex;
+width: 196px;
+padding: 14px 28px;
+justify-content: center;
+align-items: center;
+gap: 6px;
+border-radius: 10.311px;
+background: #EBD8FF;
+box-shadow: 0px 3.4369285106658936px 3.4369285106658936px 0px rgba(0, 0, 0, 0.25);
+margin: 0 auto
+`
